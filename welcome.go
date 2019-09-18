9b4f1ff083de2497a69420086ce3ba2bd2ba3c65
AldairMC <aldairmc2023@gmail.com>
2019-09-18 15:40:38 +0000

    Working with GO

---
+++ welcome.go
@@ -0,0 +1,12 @@
+// sudo apt-get update
+// sudo apt-get -y upgrade
+
+// wget https://dl.google.com/go/go1.12.7.linux-amd64.tar.gz
+
+// sudo tar -xvf go1.12.7.linux-amd64.tar.gz
+// sudo mv go /usr/local
+
+// export GOROOT=/usr/local/go
+// export GOPATH=$HOME/Projects/Proj1
+// export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+
